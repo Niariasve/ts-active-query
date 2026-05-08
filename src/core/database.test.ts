e958fb13957ab93f68b98efc07afee5426b1ea08
Niariasve <niarias@espol.edu.ec>
2026-05-07 21:54:02 -0500

    ✅ Test: add type test

--- src/core/database.test.ts
+++ src/core/database.test.ts
@@ -1,13 +1,20 @@
 import { describe, expect, it } from "vitest";
+import { expectTypeOf } from "expect-type";
 import { createDatabase } from "./database";
+import { SelectQueryBuilder } from "../query-builder/select-builder";
 import type { Database } from "./types";
 
 describe("DatabaseClient", () => {
     it('builds SQL for db.selectFrom("users").toSQL()', () => {
         const db = createDatabase<Database>();
+        const builder = db.selectFrom("users");
 
-        const sql = db.selectFrom("users").toSQL();
-        
+        expectTypeOf(builder).toEqualTypeOf<
+            SelectQueryBuilder<Database, "users">
+        >();
+
+        const sql = builder.toSQL();
+
         expect(sql).toBe("SELECT * FROM users");
     });
 });
